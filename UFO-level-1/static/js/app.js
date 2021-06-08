24acc2f1b8025725640b11969766c33ecc09e3d8
Henry Poe <hpoe111@gmail.com>
2021-06-08 18:46:07 -0400

    filter works, but no error message when the filter doesn't exist

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,8 +4,16 @@
  * Last edited on 06/08/2021
  */
 
-// Fill the table with the sighting data
-function drawTable(data, filterString) {
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+// from data.js
+var tableData = data;
+// References to the form and button
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+// Draws the table with all data on the page
+function drawTable(data) {
     data.forEach((sighting) => {
         var row = tbody.append("tr");
         Object.entries(sighting).forEach(([key, value]) => {
@@ -15,25 +23,41 @@ function drawTable(data, filterString) {
     });
 };
 
+// Draws the table with all data on the page
+function filterTable(data, filterString) {
+    // Variable to insure that at least one row is displayed after the filter
+    var count = 0;
+    data.forEach((sighting) => {
+        var row = tbody.append("tr");
+        if (sighting.datetime === filterString) {
+            Object.entries(sighting).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+            });
+            count++;
+        };
+    });
+    if (count === 0)
+        drawTable(tableData);
+};
+
 // Runs on form submit to filter the table
 function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
+    var rowCount = ufoTable.rows.length;
+    for (var i = rowCount - 1; i > 0; i--) {
+        ufoTable.deleteRow(i);
+    }
 
     var filter = d3.select(".form-control").property("value");
-    console.log(filter);
-    //drawTable(tableData, filter);
+    //console.log(filter);
+    filterTable(tableData, filter);
 };
 
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-var button = d3.select("#filter-btn");
-var form = d3.select("#form");
-
+// Run the filter function when the form is submitted or when the button is pressed
 button.on("click", runEnter);
 form.on("submit", runEnter)
+// Draw the table on initial load
+drawTable(tableData);
